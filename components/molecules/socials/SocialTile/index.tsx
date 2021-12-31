d2bfd8fe8a031b4e9c28dac4f65a25e1a2d7f1cf
Leonid Meleshin <leon.03.99@gmail.com>
2021-12-31 13:27:04 +0300

    feat(Socials): add dark theme

--- components/molecules/socials/SocialTile/index.tsx
+++ components/molecules/socials/SocialTile/index.tsx
@@ -9,13 +9,14 @@ interface Props {
 
 const IconWrapper = styled.div([
   tw`p-2 rounded mr-2`,
-  tw`group-hover:bg-gray-100`
+  tw`group-hover:bg-gray-100 dark:group-hover:bg-gray-800`
 ])
 
 const SocialTile: React.FC<Props> = ({ social }) => (
   <a href={social.url} target="_blank" rel='noopener noreferrer' className="
-    group flex flex-row items-center text-gray-600
-    hover:underline hover:text-gray-700
+    group flex flex-row items-center
+    text-gray-600 hover:text-gray-700 hover:underline
+    dark:text-gray-300 dark:hover:text-gray-200
     rounded ring-offset-2
     focus:outline-none focus:ring
   ">
