c2afa109d2ba42c3bfcba5838440531cdb603775
Leonid Meleshin <leon.03.99@gmail.com>
2022-03-11 02:37:48 +0300

    feat(Footer): add socials

--- components/organisms/TheFooter/index.tsx
+++ components/organisms/TheFooter/index.tsx
@@ -1,7 +1,10 @@
 import Link from "next/link"
 import React from "react"
+import { Icon } from '@iconify/react'
 
 import tw, { styled } from "twin.macro"
+
+import type { SocialAccount } from "../../../types/social-account"
 
 const commitSha =
   process.env.NEXT_PUBLIC_VERCEL_GIT_COMMIT_SHA ||
@@ -25,7 +28,11 @@ const FooterMenuItem: React.FC<FooterMenuItemProps> = ({ href, title, ...props }
   </li>
 )
 
-const TheFooter: React.FC = () => (
+interface Props {
+  socials: SocialAccount[]
+}
+
+const TheFooter: React.FC<Props> = ({ socials }) => (
   <Footer>
     <nav>
       <ul className="flex flex-row flex-wrap justify-center">
@@ -38,9 +45,34 @@ const TheFooter: React.FC = () => (
       </ul>
     </nav>
 
-    {/* <nav>
-      <ul></ul>
-    </nav> */}
+    <nav>
+      <ul className="flex flex-row space-x-4">
+        { socials.map((social, i) => (
+          <li
+            key={`home-social-${i}`}
+          >
+            <a
+              href={social.url}
+              target="_blank"
+              rel="noopener noreferrer"
+              className="
+                block
+                p-1
+                text-gray-500 hover:text-gray-700
+                dark:text-gray-400 hover:dark:text-gray-200
+                focus:outline-none focus:ring
+                transition-colors
+                duration-300
+                rounded ring-offset-2
+              "
+              aria-label={social.label}
+            >
+              <Icon icon={social.icon} className="block w-6 h-6" />
+            </a>
+          </li>
+        )) }
+      </ul>
+    </nav>
 
     <p className="text-xs text-gray-800 dark:text-gray-400">
       &copy; Leonid Meleshin. All rights reserved
--- layouts/index.tsx
+++ layouts/index.tsx
@@ -3,6 +3,9 @@ import { FunctionComponent } from 'react'
 import TheFooter from '../components/organisms/TheFooter'
 import TheHeader from '../components/organisms/TheHeader'
 
+// Content
+import { primarySocials } from '../content/socials'
+
 const Layout: FunctionComponent = ({ children }) => {
   return (
     <div className="">
@@ -10,7 +13,7 @@ const Layout: FunctionComponent = ({ children }) => {
       <main className='container pt-8 mx-auto'>
         {children}
       </main>
-      <TheFooter />
+      <TheFooter socials={primarySocials} />
     </div>
   )
 }
