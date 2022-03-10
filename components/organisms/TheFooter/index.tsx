9e653962d1097fc7adbef86077e1825d5438f368
Leonid Meleshin <leon.03.99@gmail.com>
2022-03-11 02:28:41 +0300

    feat(Footer) add dark theme support

--- components/organisms/TheFooter/index.tsx
+++ components/organisms/TheFooter/index.tsx
@@ -8,7 +8,8 @@ const commitSha =
   '0000000000000000000000000000000000000000'
 
 const Footer = styled.footer([
-  tw`w-full bg-gray-200 py-8 mt-19 flex flex-col justify-center items-center space-y-4`,
+  tw`w-full bg-gray-200 px-6 py-8 mt-19 flex flex-col justify-center items-center space-y-4`,
+  tw`dark:bg-gray-800`
 ])
 
 interface FooterMenuItemProps {
@@ -17,9 +18,9 @@ interface FooterMenuItemProps {
 }
 
 const FooterMenuItem: React.FC<FooterMenuItemProps> = ({ href, title, ...props }) => (
-  <li {...props}>
+  <li className="py-2 px-6" {...props}>
     <Link href={href}>
-      <a className="text-gray-600 hover:text-gray-800">{ title }</a>
+      <a className="text-gray-600 hover:text-gray-800 dark:text-gray-400 dark:hover:text-gray-300">{ title }</a>
     </Link>
   </li>
 )
@@ -27,7 +28,7 @@ const FooterMenuItem: React.FC<FooterMenuItemProps> = ({ href, title, ...props }
 const TheFooter: React.FC = () => (
   <Footer>
     <nav>
-      <ul className="flex flex-col space-x-6 md:flex-row">
+      <ul className="flex flex-row flex-wrap justify-center">
         <FooterMenuItem href="/about" title="About" />
         <FooterMenuItem href="https://blog.leon0399.ru/" title="Blog" />
         <FooterMenuItem href="/projects" title="Projects" />
@@ -41,10 +42,17 @@ const TheFooter: React.FC = () => (
       <ul></ul>
     </nav> */}
 
-    <p className="text-xs text-gray-800">
+    <p className="text-xs text-gray-800 dark:text-gray-400">
       &copy; Leonid Meleshin. All rights reserved
       {' '} &bull; {' '}
-      <a href={`https://github.com/leon0399/leon0399.ru/commit/${commitSha}`} className="font-mono hover:underline">{ commitSha.substring(0, 7) }</a>
+      <a
+        href={`https://github.com/leon0399/leon0399.ru/commit/${commitSha}`}
+        className="font-mono hover:text-gray-800 dark:hover:text-gray-300 hover:underline"
+        target="_blank"
+        rel="noreferrer"
+      >
+        { commitSha.substring(0, 7) }
+      </a>
     </p>
   </Footer>
 );
