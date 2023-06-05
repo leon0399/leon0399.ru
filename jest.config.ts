e25ad4bee5ca0dd6dca8453dc8e83578c5d6bd51
Leonid Meleshin <hello@leon0399.ru>
2023-06-05 17:46:54 +0400

    test: add jest

---
+++ jest.config.ts
@@ -0,0 +1,40 @@
+import type { Config } from '@jest/types'
+import nextJest from 'next/jest'
+
+const babelConfigEmotion = {
+  presets: [
+    [
+      'next/babel',
+      {
+        'preset-react': {
+          runtime: 'automatic',
+          importSource: '@emotion/react',
+        },
+      },
+    ],
+  ],
+  plugins: [
+    require.resolve('@emotion/babel-plugin'),
+    require.resolve('babel-plugin-twin'),
+    require.resolve('babel-plugin-macros'),
+  ],
+}
+
+const customJestConfig: Config.InitialOptions = {
+  verbose: true,
+  setupFilesAfterEnv: ['@testing-library/jest-dom/extend-expect'],
+  testEnvironment: 'jest-environment-jsdom',
+  modulePathIgnorePatterns: ['<rootDir>/dist/'],
+  testPathIgnorePatterns: ['<rootDir>/e2e'],
+  snapshotSerializers: ['@emotion/jest/serializer'],
+  moduleNameMapper: {
+    '@vercel/analytics': 'identity-obj-proxy',
+  },
+  transform: {
+    '^.+\\.(js|jsx|ts|tsx|mjs)$': ['babel-jest', babelConfigEmotion],
+  },
+}
+
+const createJestConfig = nextJest({ dir: './' })(customJestConfig)
+
+export default createJestConfig
