61b771ea40f6bc7b3878bd57eb6cd1555f3603f5
Leonid Meleshin <hello@leon0399.ru>
2025-01-17 11:58:04 +0100

    chore: hide announcements bar

--- layouts/index.tsx
+++ layouts/index.tsx
@@ -1,6 +1,6 @@
 import { type FC, type PropsWithChildren } from 'react'
 
-import TheAnnouncementBar from '../components/organisms/TheAnnouncementBar'
+// import TheAnnouncementBar from '../components/organisms/TheAnnouncementBar'
 import TheFooter from '../components/organisms/TheFooter'
 import TheHeader from '../components/organisms/TheHeader'
 // Content
@@ -9,7 +9,7 @@ import { primarySocials } from '../content/socials'
 const Layout: FC<PropsWithChildren> = ({ children }) => {
   return (
     <>
-      <TheAnnouncementBar />
+      {/* <TheAnnouncementBar /> */}
       <TheHeader />
       <main className="mx-auto pt-8">{children}</main>
       <TheFooter socials={primarySocials} />
